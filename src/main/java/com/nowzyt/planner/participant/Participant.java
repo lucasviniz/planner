b18e17cded30bf32d0a1f72f156e927a97ee96fa
Lucas Vinicius <howaitosame13@gmail.com>
2024-07-10 18:46:57 -0300

    added update and confirm request for trip and create entity participant

---
+++ src/main/java/com/nowzyt/planner/participant/Participant.java
@@ -0,0 +1,36 @@
+package com.nowzyt.planner.participant;
+
+import com.nowzyt.planner.trip.Trip;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "participant")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Participant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private Boolean isConfirmed;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+}
---
+++ src/main/java/com/nowzyt/planner/participant/ParticipantRepository.java
@@ -0,0 +1,9 @@
+package com.nowzyt.planner.participant;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ParticipantRepository extends JpaRepository<Participant, UUID> {
+
+}
